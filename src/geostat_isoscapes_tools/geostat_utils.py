26fd1f03bebcf2ff3d6c6f7993e4a42ad5afd342
Lulux4 <leagainon@gmail.com>
2025-10-27 17:36:44 +0100

    clean variograms and associated functions for plotting

---
+++ src/geostat_isoscapes_tools/geostat_utils.py
@@ -0,0 +1,129 @@
+
+from pyproj import Transformer
+import xarray as xr
+import numpy as np
+from scipy.spatial.distance import pdist
+from sklearn.linear_model import LinearRegression
+from pyproj import Geod
+import gstools as gs
+import skgstat as skg
+
+# =====================================================
+# Useful functions for geostatistical tasks 
+# =====================================================
+
+def get_projected_coords_and_vals(data_array : xr.DataArray, time : str, epsg : str = "EPSG:3857"):    
+    ''' This function extracts a slice from a xarray DataArray at a given time,
+    converts lat and lon coordinates to the chosen epsg projection, stack them to get an array of shape (n,2),
+    and returns the stacked arrray as well as the flattened values associated to each point of the coordinates array.
+    '''
+    da_slice = data_array.sel(time=time)
+    stacked = da_slice.stack(point=('lat','lon')).dropna('point')
+    lons_s = stacked['lon'].values
+    lats_s = stacked['lat'].values
+    vals_s = stacked.values.astype(float)
+
+    xs,ys = project_coords(lons_s,lats_s,epsg=epsg)
+    coords_proj = np.column_stack([xs,ys])
+    
+    return coords_proj, vals_s
+
+def project_coords(lons,lats,epsg="EPSG:3857"):
+    ''' Project lon/lat coordinates to the given epsg projection'''
+    transformer = Transformer.from_crs("EPSG:4326",epsg, always_xy=True)
+    xs, ys = transformer.transform(lons, lats)
+    return xs, ys 
+
+
+
+def detrend_data(coords, vals):
+    ''' detrend: fit plane z ~ x + y via a linear regression and subtract it to vals.
+    Returns the residuals
+    '''
+    lr = LinearRegression().fit(coords, vals)
+    trend = lr.predict(coords)
+    resid = vals - trend
+    return resid
+
+def geodesic_condensed_distances(lonlat):
+    """
+    Compute condensed vector of all pairwise geodesic distances (in meters)
+    between lon/lat points.
+    """
+    n = len(lonlat)
+    geod = Geod(ellps="WGS84")
+
+    # Upper-triangle indices for unique pairs
+    i, j = np.triu_indices(n, k=1)
+    lon1, lat1 = lonlat[i, 0], lonlat[i, 1]
+    lon2, lat2 = lonlat[j, 0], lonlat[j, 1]
+    # geod.inv returns (fwd_azimuth, back_azimuth, distance)
+    _, _, dist = geod.inv(lon1, lat1, lon2, lat2)
+    return dist
+
+def variogram_gstools(coords, 
+                      vals, 
+                      n_bins : int, 
+                      maxlag : float|None = None,
+                      maxlag_factor : float = 0.65 
+                      ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    ''' Computes variogram using gstools vario_estimate function. 
+    Euclidean distances are used.
+    '''
+    # compute pairwise distances 
+    dists = pdist(coords)
+    
+    # define max lag
+    if maxlag is None:
+        maxlag_ = dists.max() * maxlag_factor
+    else :
+        maxlag_ = maxlag
+
+    # define bins **EDGES**
+    bins_edges = np.linspace(0, maxlag_, n_bins + 1)
+    
+    # gstools variogram computation
+    bin_centers, gamma, counts = gs.vario_estimate(coords.T, vals, bin_edges = bins_edges, return_counts = True)
+    
+    return bin_centers, gamma, counts
+
+
+def variogram_with_gstat(df,
+                         quantity:str,
+                         sample_size=3000,
+                         trend_removal=False, 
+                         nlags : int = 30, 
+                         maxlag : float | str | None = 'median', 
+                         model : str ='spherical',
+                         return_Variogram_object=False,
+                         seed = 42
+                         )-> skg.Variogram | tuple[np.ndarray,np.ndarray, np.ndarray, object] :
+    """Compute experimental variogram with sampling.
+    TODO : write this fct doc 
+    """
+    # check if the month contains too much samples. If so, sample it down to the sample_size.
+    if len(df) > sample_size:
+        print('downsampling')
+        df = df.sample(sample_size, random_state=seed)
+    
+    vals = df[quantity].values
+    
+    if trend_removal :
+        # Remove trend by fitting a plane 
+        XY = np.column_stack((df['x'], df['y']))
+        vals = detrend_data(XY,df[quantity].values)
+
+    # Compute the variogram 
+    V = skg.Variogram(
+        df[['x','y']].values,
+        vals,
+        n_lags=nlags,
+        normalize=True,
+        maxlag=maxlag,
+        model=model,
+        use_nugget=True
+    )
+    if return_Variogram_object:
+        return V
+    else :
+        return V.bins, V.experimental, V.bin_count,V.fitted_model
---
+++ src/geostat_isoscapes_tools/plot_utils.py
@@ -0,0 +1,82 @@
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt 
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+import xarray as xr
+
+def plot_isoscape_latlon_platecarree(dataarray_slice: xr.DataArray, time: str, title :str = 'Values of d18Op')-> tuple[Figure,Axes]:
+    ''' Plot the given isotope data array slice in platecarree projection.
+    '''
+    fig, ax = plt.subplots(figsize=(15, 8), subplot_kw={"projection": ccrs.PlateCarree()})
+    
+    dataarray_slice.plot(
+        ax=ax,
+        transform=ccrs.PlateCarree(),
+        cmap="viridis",
+        cbar_kwargs={"label": dataarray_slice.name},
+        robust=True
+    ) #type: ignore
+
+    ax.coastlines() # type: ignore
+    # title with the year and the month
+    ax.set_title(f"{title} - {time}")
+    return fig, ax
+
+def plot_projected_data(coords_proj,vals_s, projection_str='Mercator'):
+    if projection_str == 'Mercator':
+        projection = ccrs.Mercator()
+    
+    fig, ax = plt.subplots(subplot_kw={"projection": projection})
+    sc = ax.scatter(coords_proj[:,0],coords_proj[:,1], c=vals_s, s=20, cmap="viridis", transform=projection, alpha=0.9, edgecolor="k", linewidth=0.1)
+    ax.coastlines(resolution="50m") # type: ignore
+    ax.set_title(f"d18Op values ({projection_str})")
+    plt.colorbar(sc, ax=ax, label="d18Op")
+        
+    return fig, ax
+
+def plot_variogram_from_bins_and_gamma(centers,
+                                       gamma,
+                                       time, 
+                                       counts = None, 
+                                       min_pairs=30, 
+                                       plot_model : bool = True, 
+                                       model_name : str|None = None, 
+                                       model_fct = None,
+                                       figsize: tuple[int,int]=(10,5)
+                                       ) -> tuple[Figure,Axes]:
+    ''' Plot an empirical variogram from given bin centers and semivariances values.
+    Overlays pairs number per bin if counts is given. Does not plot bins with less than min_pairs if counts is given.
+    '''
+    if counts is not None :
+        reliable = counts >= min_pairs
+        centers = centers[reliable]
+        gamma = gamma[reliable]
+        if counts is not None:
+            counts = counts[reliable]
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(centers/1000, gamma, 'o--', color='C0',linewidth=1,markersize=4)
+    ax.set_xlabel("Lag distance (km)")
+    ax.set_ylabel("Semivariance")
+
+    # Overlay model if wanted and given
+    if (plot_model) and (model_name is not None) and (model_fct is not None):
+        ax.plot(centers/1000,model_fct(centers),'-', color='C1', linewidth=1, label=f'{model_name} fit')
+        plt.legend()
+    
+    width = (centers[1]-centers[0])/1000
+    # Overlay bins counts if given
+    if counts is not None:
+        # Overlay pair counts
+        ax2 = ax.twinx()
+        ax2.bar(centers/1000, counts, width = width,
+                color='red', alpha=0.1, label='Pair counts')
+        ax2.set_ylabel('Number of pairs', color='red')
+        ax2.tick_params(axis='y', labelcolor='red')
+    
+    plt.title(f"Empirical variogram — {time}")
+    plt.xlim(0, centers.max()/1000+width)
+    plt.grid(True, alpha=0.4)
+    plt.show()
+
+    return fig,ax
