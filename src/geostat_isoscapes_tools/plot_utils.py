fixed pb of lon/lat columns names by setting it as a parameter, and implemented masking around (sisal) points

src/geostat_isoscapes_tools/plot_utils.py
@@ -129,7 +129,8 @@ def plot_variogram_from_bins_and_gamma(centers,
                                        model_params = None,
                                        figsize: tuple[int,int]=(10,5),
                                        ax_ = None,
-                                       save_name : str | None = None
+                                       save_name : str | None = None,
+                                       verbose : bool =  False
                                        ) -> tuple[Figure,Axes] | Axes :
     """ Plot an empirical variogram from given bin centers and semivariances values.
     Overlays pairs number per bin if counts is given. Does not plot bins with less than min_pairs if counts is given.
@@ -150,19 +151,20 @@ def plot_variogram_from_bins_and_gamma(centers,
 
     # Overlay model if wanted and given
     if (plot_model) and (model_name is not None) and (model_fct is not None):
+        if verbose : print('plotting model')
         h = np.arange(0,centers.max(),10000)
-        legend_model = model_name if model_name!='composite' else 'spherical+gaussian'
+        legend_model = model_name 
         ax.plot(h,model_fct(h),'-', color='C1', linewidth=1, label=f'{legend_model} fit')
         sill,range_,nugget= None,None,None
         
-        if (model_name != 'composite') and (model_params is not None) :
+        if (not ('+' in model_name)) and (model_params is not None) :
             range_ = model_params['range']
             sill = model_params['sill']+model_params['nugget']
             nugget = model_params['nugget']
             ax.vlines(range_,0,max(gamma),color="#FF1E00",linestyle='--',alpha=0.2,label=f"range")
             ax.hlines(sill,0,max(centers),color="#BD6D12",linestyle='--',alpha=0.2,label=f"sill")
         
-        elif (model_name =='composite') and (model_params is not None) :
+        elif ('+' in model_name) and (model_params is not None) :
             range_ = gutils.effective_range(centers,model_fct,0.95)
             sill = model_params['sill1']+model_params['sill2']+model_params['nugget']
             nugget= model_params['nugget']
@@ -253,15 +255,17 @@ def plot_global_map(data:pd.DataFrame,
                     quantity:str='d18O',
                     unit:str='‰ VPDB',
                     proj:bool=True,
-                    symbol : str ='square')-> go.Figure:
+                    symbol : str ='square',
+                    lon_col : str = 'longitude',
+                    lat_col : str = 'latitude')-> go.Figure:
     ''' 3D or flat earth (natural earth proj)
     If proj=True : 2D 
     else : 3D
     '''
     fig = go.Figure()
     fig.add_trace(go.Scattergeo(
-        lon=data["longitude"],
-        lat=data["latitude"],
+        lon=data[lon_col],
+        lat=data[lat_col],
         text=data[quantity_col],
         mode="markers",
         marker=dict(

src/geostat_isoscapes_tools/sisal_utils.py
@@ -353,7 +353,14 @@ def convert_calcite_to_drip_water(calcite_df : DataFrame) -> DataFrame :
         # print(f"   for mineralogy {mineralogy}, the conversion failed for {converted_data.loc[mask,'d18Op_VSMOW'].isna().sum()} samples.")
     return converted_data
 
-def retrieve_T_RegularGridInterp( data_df : DataFrame, temp_xda : DataArray, method : str = 'linear') -> DataFrame :
+def retrieve_T_RegularGridInterp( data_df : DataFrame, 
+                                 temp_xda : DataArray, 
+                                 method : str = 'linear',
+                                 data_lon_col='longitude',
+                                 data_lat_col='latitude',
+                                 temp_lon_col='lon',
+                                 temp_lat_col='lat'
+                                 ) -> DataFrame :
     ''' Retrieve the temperature of each sample of data_df by interpolating temp_xda values at data_df points using scipy RegularGridInterpolator
     Inputs :
         - data_df : DataFrame containing columns 'latitude', ' longitude', chrono. The chrono column should contain **positive** ages in yrs BP
@@ -364,8 +371,8 @@ def retrieve_T_RegularGridInterp( data_df : DataFrame, temp_xda : DataArray, met
         - data_df : with an exra column 'T_interp' containing the temperature associated to each sample row.
     '''
     # set up the interpolator
-    temp_lats  = temp_xda['lat'].values.copy()
-    temp_lon   = temp_xda['lon'].values.copy()
+    temp_lats  = temp_xda[temp_lat_col].values.copy()
+    temp_lon   = temp_xda[temp_lon_col].values.copy()
     temp_times = temp_xda['time'].values.copy()
     temp_values = temp_xda.values
 
@@ -379,8 +386,8 @@ def retrieve_T_RegularGridInterp( data_df : DataFrame, temp_xda : DataArray, met
 
     # set up sample points for interpolation
     sample_times = - data_df['age'].values # type: ignore
-    sample_lats = data_df['latitude'].values
-    sample_lons = data_df['longitude'].values
+    sample_lats = data_df[data_lat_col].values
+    sample_lons = data_df[data_lon_col].values
 
     points = np.column_stack([sample_times, sample_lats, sample_lons]) # type: ignore
 
@@ -420,23 +427,23 @@ def retrieve_temperature_and_convert_speleothem_d18O(data_df : DataFrame, temp_x
 
     return converted_data
 
-def retrieve_continent_from_lat_lon(df_orig : pd.DataFrame) -> pd.DataFrame :
+def retrieve_continent_from_lat_lon(df_orig : pd.DataFrame,lat_col :str ='latitude',lon_col :str ='longitude') -> pd.DataFrame :
     ''' TODO : explain this rough def of continents
     '''
     df = df_orig.copy()
     # Define regions
     df['continent'] = ''
     #middle east apart form africa :
-    df.loc[(df['latitude']>= -35)&(df['latitude']<= 37)&(df['longitude']>= -20)&(df['longitude']<= 52),'continent']='Africa'
-    df.loc[(df['latitude']<= -60),'continent']='Antarctica'
-    df.loc[(df['latitude']>= 5)&(df['latitude']<= 81)&(df['longitude']>= 26)&(df['longitude']<= 180),'continent']='Asia'
-    df.loc[(df['latitude']>= 35)&(df['latitude']<= 72)&(df['longitude']>= -25)&(df['longitude']<= 50),'continent']='Europe'
-    df.loc[(df['latitude']>= 5)&(df['latitude']<= 83)&((df['longitude']>= -170)&(df['longitude']<= -50)),'continent']='North America'
-    df.loc[(df['latitude']>= -50)&(df['latitude']<= 0)&((df['longitude']>= 110)|(df['longitude']<= 180)),'continent']='Oceania'
-    df.loc[(df['latitude']>= -60)&(df['latitude']<= 15)&(df['longitude']>= -90)&(df['longitude']<= -30),'continent']='South America'
-    df.loc[(df['continent']=='') & (df['latitude']>= 0) & (df['latitude']<= 15) & (df['longitude']>= 110) & (df['longitude']<= 120),'continent']='Indonesia' # Indonesie, transcontinental
-    df.loc[(df['continent']=='') & (df['latitude']>= 75) & (df['latitude']<= 85) & (df['longitude']>= -30) & (df['longitude']<= -15),'continent']='North America' # Greenland is on North America plate
-    df.loc[(df['latitude']>= 12)&(df['latitude']<= 42)&(df['longitude']>= 32)&(df['longitude']<= 60),'continent']='Middle East' # Middle East
+    df.loc[(df[lat_col]>= -35)&(df[lat_col]<= 37)&(df[lon_col]>= -20)&(df[lon_col]<= 52),'continent']='Africa'
+    df.loc[(df[lat_col]<= -60),'continent']='Antarctica'
+    df.loc[(df[lat_col]>= 5)&(df[lat_col]<= 81)&(df[lon_col]>= 26)&(df[lon_col]<= 180),'continent']='Asia'
+    df.loc[(df[lat_col]>= 35)&(df[lat_col]<= 72)&(df[lon_col]>= -25)&(df[lon_col]<= 50),'continent']='Europe'
+    df.loc[(df[lat_col]>= 5)&(df[lat_col]<= 83)&((df[lon_col]>= -170)&(df[lon_col]<= -50)),'continent']='North America'
+    df.loc[(df[lat_col]>= -50)&(df[lat_col]<= 0)&((df[lon_col]>= 110)|(df[lon_col]<= 180)),'continent']='Oceania'
+    df.loc[(df[lat_col]>= -60)&(df[lat_col]<= 15)&(df[lon_col]>= -90)&(df[lon_col]<= -30),'continent']='South America'
+    df.loc[(df['continent']=='') & (df[lat_col]>= 0) & (df[lat_col]<= 15) & (df[lon_col]>= 110) & (df[lon_col]<= 120),'continent']='Indonesia' # Indonesie, transcontinental
+    df.loc[(df['continent']=='') & (df[lat_col]>= 75) & (df[lat_col]<= 85) & (df[lon_col]>= -30) & (df[lon_col]<= -15),'continent']='North America' # Greenland is on North America plate
+    df.loc[(df[lat_col]>= 12)&(df[lat_col]<= 42)&(df[lon_col]>= 32)&(df[lon_col]<= 60),'continent']='Middle East' # Middle East
 
     continents = df['continent'].unique()
     # print('-> Continents found in the site df :',continents)

src/geostat_isoscapes_tools/utils.py
@@ -4,9 +4,10 @@ import geopandas as gpd
 import numpy as np 
 import shapely 
 from pyproj import CRS
+from scipy.spatial.distance import cdist
 
 # =========================================================================================
-# MISC.TOOLS FOR DATA LOADING, BINNING, MASKING
+# MISC.TOOLS FOR DATA LOADING, BINNING
 # =========================================================================================
 def load_xarray_datarray(fn: str) -> xr.Dataset:
     """ Just a function to open a netcdf dataset """
@@ -40,6 +41,10 @@ def bin_xrDataArray_time(da : xr.DataArray, res) -> xr.DataArray :
 def get_yrBP_from_itrace_time(days_after_start : int, start_year : int = 11700)->float:
     """ TODO"""
     return start_year + days_after_start / 365.0
+
+# =============================================================================================
+# "Spatial" computations
+# =============================================================================================
 
 def mask_country_shape(da : xr.DataArray | pd.DataFrame, 
                        country_names: list[str] = ["China"], 
@@ -112,6 +117,65 @@ def convert_lat_0_180_to_neg90_90(lat:np.ndarray) ->np.ndarray:
     """ Converts latitudes in the interval 0 to 180 degreees to -90 to 90 degrees"""
     return (lat + 90) % 180 -90
 
+def haversine(u, v):
+    """Haversine distance (km) between two (lat,lon) points."""
+    lat1, lon1 = np.radians(u)
+    lat2, lon2 = np.radians(v)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    return 2 * 6371 * np.arcsin(np.sqrt(a)) # since earth radius = approx 6371 km
+
+
+def mask_union_of_circles_around_pts(data_to_mask : pd.DataFrame | xr.Dataset | xr.DataArray,
+                                     df_ref_pts : pd.DataFrame, 
+                                     radius_km : int,
+                                     lat_name="lat", 
+                                     lon_name="lon",):
+    """ Masks points in dataframe or xarray dataset, keeping only the union of circles centered on df_ref_pts points, with a radius radius_km. 
+    """
+
+    # get the list of (unique) ref points locations 
+    unique_locs = df_ref_pts[[lat_name,lon_name]].drop_duplicates()
+    ref_locs = unique_locs.values
+    # ===============
+    # pandas version
+    # ===============
+    if isinstance(data_to_mask,pd.DataFrame):
+        # data points of the df to mask
+        locs_to_mask = data_to_mask[[lat_name,lon_name]].drop_duplicates().reset_index(drop=True)
+        # Compute min distance from each point to nearest ref point
+        distances = cdist(locs_to_mask[[lat_name,lon_name]].values, ref_locs, metric=haversine)  # shape (n_unique_to_mask, n_ref_pts)
+        # Mask df
+        locs_to_mask['mask'] = distances.min(axis=1)  <= radius_km
+
+        return apply_spatial_mask(data_to_mask,locs_to_mask,lat_name,lon_name,'mask'), locs_to_mask
+    # ===============
+    # xr version 
+    # ===============
+    elif isinstance(data_to_mask,xr.Dataset):
+        # points to mask (extraction)
+        lats = data_to_mask[lat_name].values
+        lons = data_to_mask[lon_name].values
+        lon2d, lat2d = np.meshgrid(lons,lats)
+        locs = np.column_stack([lat2d.ravel(), lon2d.ravel()])  # (nlat*nlon, 2)
+        # compute dist
+        distances = cdist(locs, ref_locs, metric=haversine)  # (npts, Nref)
+        min_dist = distances.min(axis=1).reshape(lat2d.shape)  # reshape to (lat,lon)
+        # mask
+        mask2d = min_dist <= radius_km    # (lat, lon)
+        mask = xr.DataArray(mask2d,
+                            dims=(lat_name, lon_name),
+                            coords={lat_name: data_to_mask[lat_name], lon_name: data_to_mask[lon_name]},
+                        ).expand_dims(time=data_to_mask.time)
+        
+        return data_to_mask.where(mask), mask
+
+
+def apply_spatial_mask(df, spatial_mask, lat='lat', lon='lon',mask_col='mask'):
+    """ Applies a mask defined on specific lat lon coordinates to a dataframe """
+    merged = df.merge(spatial_mask, on=[lat, lon], how="left")    
+    return merged[merged[mask_col]]
 # =========================================================================================
 # STATISTICAL METRICS
 # =========================================================================================